ed6a1f2ba265142a2f7c0fbe1fb86150aa1684b1
onur <ionuraygan@gmail.com>
2022-12-28 02:32:21 +0300

    Character card component

---
+++ components/CharacterCard/CharacterCard.tsx
@@ -0,0 +1,45 @@
+import React, { use, useEffect, useState } from "react";
+import styles from "./index.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+const CharacterCard = ({ data }) => {
+  return (
+    <>
+      <Link href={`/character/${data.id}`} className={styles.link}>
+        <div className={styles.wrapper}>
+          <Image
+            src={data.image}
+            width={300}
+            height={300}
+            alt={data.name}
+            className={styles.imageWrapper}
+          />
+          <div className={styles.characterName}>
+            <h1>{data.name}</h1>
+          </div>
+          <div className={styles.subHeader}>
+            {data.status === "Alive" ? (
+              <div className={styles.alive}></div>
+            ) : (
+              ""
+            )}
+            {data.status === "Dead" ? <div className={styles.dead}></div> : ""}
+            {data.status === "unknown" ? (
+              <div className={styles.unknown}></div>
+            ) : (
+              ""
+            )}
+            <div className={styles.types}>
+              <h3>
+                {data.status} - {data.species}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default CharacterCard;
