81a2ad6c894979ed446f21041bf0f6af07363f90
onur <ionuraygan@gmail.com>
2022-12-30 01:53:08 +0300

    Header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Logo from "../../assets/icons/Logo.svg";
+import styles from "./index.module.scss";
+import { useRouter } from "next/router";
+import LeftArrow from "../../assets/icons/leftArrow.svg";
+
+const Header = () => {
+  const router = useRouter();
+  console.log(router.query);
+
+  return (
+    <>
+      <div className={styles.wrapper}>
+        {router.asPath.includes("/character") ? (
+          <div className={styles.back} onClick={() => router.back()} >
+            <LeftArrow />
+          </div>
+        ) : (
+          ""
+        )}
+        <div className={styles.logo} onClick={() => router.replace("/")}>
+          <Logo />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
