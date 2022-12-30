8c18ba8341e8268dc190aad67e48f2d22d4883d7
onur <ionuraygan@gmail.com>
2022-12-30 13:12:53 +0300

    adding types and interfaces

---
+++ types/types.ts
@@ -0,0 +1,63 @@
+export enum Gender {
+    Female = "Female",
+    Male = "Male",
+    Genderless = "Genderless",
+    Unknown = "unknown",
+}
+
+export enum Species {
+    Alien = "Alien",
+    Human = "Human",
+}
+
+export enum Status {
+    Alive = "Alive",
+    Dead = "Dead",
+    Unknown = "unknown",
+}
+
+export interface Info {
+    count: number;
+    pages: number;
+    next: string;
+    prev: null;
+}
+
+export interface Character {
+    id: number;
+    name: string;
+    status: Status;
+    species: Species;
+    type: string;
+    gender: Gender;
+    origin: Location;
+    location: Location;
+    image: string;
+    episode: string[];
+    url: string;
+    created: Date;
+}
+
+export interface GetCharacterResults {
+    info: Info;
+    results: Character[];
+}
+
+export interface OtherCharactersResults {
+    results: Character[]
+}
+
+export interface Location{
+    name: string;
+    url: string
+}
+
+export interface LocationType {
+    id: number;
+    name: string;
+    dimension: string;
+    type: string;
+    url: string;
+    created: Date;
+    residents: string[];
+}
