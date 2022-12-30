6c7383e12eeb2ee7e701f4a47637d1dd1f713a92
onur <ionuraygan@gmail.com>
2022-12-30 15:47:04 +0300

    Filter component

---
+++ components/Filter/Filter.tsx
@@ -0,0 +1,56 @@
+import React, { FC, useEffect, SetStateAction } from "react";
+import { Character, FilterProps } from "../../types/types";
+import styles from "./filter.module.scss";
+
+const Filter: FC<FilterProps> = ({
+  datas,
+  filtered,
+  setFiltered,
+  status,
+  setStatus,
+  allCharacters,
+}) => {
+  useEffect(() => {
+    if (status === "All") {
+      setFiltered(allCharacters);
+      return;
+    }
+    const filteredCharacters = datas.filter((character: Character) => {
+      return character.status === status;
+    });
+    setFiltered(filteredCharacters);
+    console.log(status);
+  }, [status]);
+
+  return (
+    <div className={styles.wrapper}>
+      <h3>Filter by status:</h3>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.allButton} onClick={() => setStatus("All")}>
+          <div className={styles.allStatus}></div>
+          All
+        </button>
+        <button className={styles.deadButton} onClick={() => setStatus("Dead")}>
+          <div className={styles.deadStatus}></div>
+          Dead
+        </button>
+        <button
+          className={styles.aliveButton}
+          onClick={() => setStatus("Alive")}
+        >
+          <div className={styles.aliveStatus}></div>
+          Alive
+        </button>
+        <button
+          className={styles.unknownButton}
+          onClick={() => setStatus("unknown")}
+        >
+          <div className={styles.unknownStatus}></div>
+          Unknown
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
